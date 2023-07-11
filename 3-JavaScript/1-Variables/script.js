29bc793a5ec1a686c7e9c3a723bc05ae4171b39a
Anantha Ramakrishnan Subramanian <anantharamakrishnan19@gmail.com>
2023-07-11 10:21:50 +0530

    Learning Javascript

---
+++ 3-JavaScript/1-Variables/script.js
@@ -0,0 +1,59 @@
+
+// console.log(y)
+// var y
+
+// // let y =10
+// y=15
+// console.log(y)
+// const z=15
+
+
+
+// const num = 1;
+// console.log("num",typeof num);
+
+// const str = "1";
+// console.log("str",typeof str);
+
+// const bool = true;
+// console.log("bool",typeof bool);
+
+// let x;
+// console.log("x",typeof x);
+
+// const y = null;
+// console.log("y",typeof y);
+
+// const arr = ["Apple", "Banana", "Orange"];
+// console.log("arr",typeof arr);
+
+// const obj = {firstName: "John", lastName: "Smith"};
+// console.log("obj",typeof obj);
+
+// const fn = function test(){return {firstName: "John", lastName: "Smith"}};
+// console.log("fn",typeof fn);
+
+
+// let x = 17;
+// let y = 4;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+
+// let grade = 95;
+// grade = 100;
+// console.log("Grade:", grade);
+
+// let studentName = "Stan";
+// studentName = "Stanley";
+// console.log("Name:",studentName);
+
+// console.log(1 == 1);
+// console.log(2 > 6);
+// console.log(1 == "1");
+// console.log(1 === "1");
+// console.log(5 != 10);
